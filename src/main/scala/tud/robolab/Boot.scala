a903c508cdd9ed0aa437bcbc190e3825eb137ac9
Max Leuthaeuser <max.leuthaeuser@gmail.com>
2013-08-16 18:21:47 +0200

    - added missing Boot object

---
+++ src/main/scala/tud/robolab/Boot.scala
@@ -0,0 +1,21 @@
+package tud.robolab
+
+import akka.actor.{ActorSystem, Props}
+import akka.io.IO
+import spray.can.Http
+import com.typesafe.config.ConfigFactory
+
+object Boot extends App {
+  val conf = ConfigFactory.load("application.conf")
+  lazy val IP = conf.getString("spray.can.server.ip")
+  lazy val PORT = conf.getInt("spray.can.server.port")
+
+  // we need an ActorSystem to host our application in
+  implicit val system = ActorSystem("on-spray-can")
+
+  // create and start our service actor
+  val service = system.actorOf(Props[SimulationServiceActor], "sim-service")
+
+  // start a new HTTP server on port 8080 with our service actor as the handler
+  IO(Http) ! Http.Bind(service, interface = IP, port = PORT)
+}
