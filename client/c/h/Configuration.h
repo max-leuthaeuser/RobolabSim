96e2fb92aae7cd9a38b07e91e29de79a0f4eee42
Max Leuthäuser <max.leuthaeuser@gmail.com>
2014-02-13 09:03:57 +0100

    - made group ID config more explicit

--- client/c/h/Configuration.h
+++ client/c/h/Configuration.h
@@ -13,8 +13,15 @@
 #include "Urlcode.h"
 #include "RobotProxy.h"
 
-// define server URL here
-#define URL "http://localhost:8080/query?id=someGroupID&values="
+// define server config here
+#define IP "localhost"
+#define PORT "8080"
+
+// define your GROUP ID here (no umlauts!)
+#define GROUPID "someGroupID"
+
+// define server URL
+#define URL "http://" IP ":" PORT "/query?id=" GROUPID "&values="
 
 // define maze size here
 #define MAZE_WIDTH 6
