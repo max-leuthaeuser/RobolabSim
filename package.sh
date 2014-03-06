ddad088cafaf13cd2c2670f84b206a1cbcf7b19b
Popoklopsi <witche3@googlemail.com>
2014-03-06 01:35:46 +0100

    Fixed conflicts

--- package.sh
+++ package.sh
@@ -6,8 +6,6 @@ echo "*** Copying to builds/ ***"
 cp target/scala-2.10/RobolabSim.jar builds/RobolabSim-latest.jar
 echo "*** Copying to package/ ***"
 cp target/scala-2.10/RobolabSim.jar ../package/RobolabSim.jar
-
-<<<<<<< HEAD
 cd ../testing/
 echo "*** Building testing jar ***"
 sbt assembly
@@ -16,8 +14,6 @@ cp target/scala-2.10/RobolabSimTest.jar builds/RobolabSimTest-latest.jar
 echo "*** Copying to package/ ***"
 cp target/scala-2.10/RobolabSimTest.jar ../package/RobolabSimTest.jar
 
-=======
->>>>>>> upstream/master
 cd ..
 echo "*** Copying content from client/ to package/ ***"
 cp -r client/* package/solution
